c23656e8a2c6b6636d4710f731e5df43973a084f
NestorSanchez <nesaenz.69@gmail.com>
2022-07-26 15:59:20 -0500

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { Header } from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Productos } from "./components/Productos";
+import { NuevoProducto } from "./components/NuevoProducto";
+import { EditarProducto } from "./components/EditarProducto";
 
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <div className="container mt-5">
+        <Routes>
+          <Route path="/" element={<Productos />} />
+          <Route path="/productos/nuevo" element={<NuevoProducto />} />
+          <Route path="/productos/editar/:id" element={<EditarProducto />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 };
---
+++ src/components/NuevoProducto.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const NuevoProducto = () => {
+  return (
+    <div className="row justify-content-center">
+      <div></div>
+    </div>
+  );
+};
